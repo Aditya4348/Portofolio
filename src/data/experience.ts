5925806b3499177ab06a5728fadd6de4c3f0e40c
aditya alfito <adityaalfito4348@gmail.com>
2025-11-25 21:28:06 +0700

    { ADD } Project and experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,59 +1,98 @@
-
 export const experience = {
-    title: 'Experience',
-    description: "My professional journey and the skills I've developed along the way",
-    titleWorks: 'Work Experience',
-    titleSkills: 'Skills & Technologies ',
-    jobs: [
-        {
-            id: "1",    
-            title: "internship Wordpress Developer",
-            company: "Jasawebsite.biz",
-            period: "2024 Sept - 2024 Dec",
-            description: "Internship as a Frontend Developer at Jasawebsite.biz with a focus on web development using WordPress and maintenance for various client projects.",
-            detaildescription: "",
-            mainImage: "/PKL/IMG_20241122_144114.jpg",
-            images: [
-
-            ],
-            buttons: [{ label: "View Project", category: "experience", id: "1" }],
-        },
-        {
-            id: "2",
-            title: "1st Place in Student Exhibition",
-            company: "SMK BPPI Baleendah",
-            period: "2025",
-            description: "1st Place in Student Exhibition with School Savings App. Acted as Frontend Developer and presentation mediator",
-            detaildescription: "",
-            mainImage: "/Pameran/main.jpg",
-            images: [
-                { id: "1", img: "Pameran/1_(17).jpg", url: "Programmer", height: 400 },
-                { id: "2", img: "Pameran/1_(16).jpg", url: "Programmer", height: 400 },
-                { id: "3", img: "Pameran/1_(15).jpg", url: "Programmer", height: 400 },
-                { id: "4", img: "Pameran/1_(14).jpg", url: "Programmer", height: 400 },
-                { id: "5", img: "Pameran/1_(13).jpg", url: "Programmer", height: 400 },
-                { id: "6", img: "Pameran/1_(12).jpg", url: "Programmer", height: 400 },
-                { id: "7", img: "Pameran/1_(11).jpg", url: "Programmer", height: 400 },
-            ],
-            buttons: [{ label: "View Project", category: "experience", id: "2" }],
-
-        },
-    ],
-    skills: [
-        {
-            id: "1",
-            category: "Web Programming",
-            skill: ["React", "Vue.JS", "Tailwind CSS", "HTML", "CSS", "JavaScript", "Bootstrap","PHP", "Laravel", "MySQL"],
-        },
-        {
-            id: "2",
-            category: "Soft Skill",
-            skill: ["Communication", "Public Speaking", "Teamwork", "Problem Solving", "Adaptability"],
-        },
-        {
-            id: "3",
-            category: "Tools & Others",
-            skill: ["Git", "Canva", "VS Code", "Microsoft Office", "Xampp", "Laragon",],
-        },
-    ],
-}
+  title: "Experience",
+  description:
+    "My professional journey and the skills I've developed along the way",
+  titleWorks: "Work Experience",
+  titleSkills: "Skills & Technologies ",
+  jobs: [
+    {
+      id: "1",
+      title: "internship Wordpress Developer",
+      company: "Jasawebsite.biz",
+      period: "2024 Sept - 2024 Dec",
+      description:
+        "Internship as a Frontend Developer at Jasawebsite.biz with a focus on web development using WordPress and maintenance for various client projects.",
+      detaildescription: "",
+      mainImage: "/PKL/IMG_20241122_144114.jpg",
+      images: [],
+      buttons: [{ label: "View Project", category: "experience", id: "1" }],
+    },
+    {
+      id: "2",
+      title: "Web Developer",
+      company: "Optibis.id",
+      period: "2025 Sept - Present",
+      description:
+        "Working as a Web Developer at Optibis.id, responsible for building and maintaining web applications, improving UI/UX, and ensuring optimal performance.",
+      detaildescription: "",
+      mainImage: "/Optibis/main.jpg", // bisa diubah sesuai gambar utama
+      images: [
+        { id: "1", img: "Optibis/1.jpg", url: "Web Developer", height: 400 },
+        { id: "2", img: "Optibis/2.jpg", url: "Web Developer", height: 400 },
+        // tambahkan jika ada gambar lain
+      ],
+      buttons: [{ label: "View Project", category: "experience", id: "2" }],
+    },
+    {
+      id: "3",
+      title: "1st Place in Student Exhibition",
+      company: "SMK BPPI Baleendah",
+      period: "2025",
+      description:
+        "1st Place in Student Exhibition with School Savings App. Acted as Frontend Developer and presentation mediator",
+      detaildescription: "",
+      mainImage: "/Pameran/main.jpg",
+      images: [
+        { id: "1", img: "Pameran/1_(17).jpg", url: "Programmer", height: 400 },
+        { id: "2", img: "Pameran/1_(16).jpg", url: "Programmer", height: 400 },
+        { id: "3", img: "Pameran/1_(15).jpg", url: "Programmer", height: 400 },
+        { id: "4", img: "Pameran/1_(14).jpg", url: "Programmer", height: 400 },
+        { id: "5", img: "Pameran/1_(13).jpg", url: "Programmer", height: 400 },
+        { id: "6", img: "Pameran/1_(12).jpg", url: "Programmer", height: 400 },
+        { id: "7", img: "Pameran/1_(11).jpg", url: "Programmer", height: 400 },
+      ],
+      buttons: [{ label: "View Project", category: "experience", id: "3" }],
+    },
+  ],
+  skills: [
+    {
+      id: "1",
+      category: "Web Programming",
+      skill: [
+        "React",
+        "Vue.JS",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Bootstrap",
+        "PHP",
+        "Laravel",
+        "MySQL",
+      ],
+    },
+    {
+      id: "2",
+      category: "Soft Skill",
+      skill: [
+        "Communication",
+        "Public Speaking",
+        "Teamwork",
+        "Problem Solving",
+        "Adaptability",
+      ],
+    },
+    {
+      id: "3",
+      category: "Tools & Others",
+      skill: [
+        "Git",
+        "Canva",
+        "VS Code",
+        "Microsoft Office",
+        "Xampp",
+        "Laragon",
+      ],
+    },
+  ],
+};
--- src/data/project.ts
+++ src/data/project.ts
@@ -1,56 +1,75 @@
 export const ContentProject = {
-    title: 'Featured Projects',
-    description: 'Here are some of my recent projects that showcase my skills and experience.',
-}
+  title: "Featured Projects",
+  description:
+    "Here are some of my recent projects that showcase my skills and experience.",
+};
 
 export const DataProject = [
-    {
-        id: "1",
-        title: "Todolist Web",
-        description: "A dynamic Todo List application built with Laravel Livewire and Tailwind CSS for real-time task management.",
-        detaildescription: "This Todo List app was developed as a modern task management solution, focused on performance, usability, and responsive design. Built with Laravel Livewire, it delivers real-time interactivity without the need for additional JavaScript.All core features such as adding, editing, completing, and deleting tasks are handled without page reloads, ensuring a fast and seamless user experience — ideal for individuals or small teams.The UI is styled using Tailwind CSS, resulting in a clean, minimalist interface that looks great on all screen sizes. Responsiveness and speed were key priorities throughout the development.This project also serves as an exploration into building backend-driven SPA-like applications without relying on frontend frameworks like React or Vue. It showcases how Laravel can power interactive, modern web apps on its own.More than just a productivity tool, this app is a demonstration of my ability to build scalable, interactive web applications with clean architecture and modern tech stacks.",
-        mainImage: "TodolistWeb.png",
-        images: [
-            ],
-        tags: ["Laravel", "LiveWire", "HTML", "CSS", "Tailwind"],
-        github: "https://github.com/Aditya4348/Todolist",
-        detail: [{category: "project", id: "1"}],
-    },
-    {
-        id: "2",
-        title: "Slicing Web ( React )",
-        description: "A modern website layout sliced and built using React.js, component reusability, and clean UI architecture.",
-        detaildescription: "A modern website built using React and TypeScript technologies, with a focus on component reusability and clean UI architecture.",
-        mainImage: "SlicingReact.png",
-        images: [
-            ],
-        tags: ["React", "Tailwind CSS", "HTML", "CSS", "Typescript", "Vite"],
-        github: "https://github.com/Aditya4348/SlicingWebReact",
-        detail: [{category: "project", id: "2"}],
-    },
-    {
-        id: "3",
-        title: "Slicing Web ( Vue JS )",
-        description: "A responsive website layout sliced and implemented using Vue.js and Tailwind CSS, focusing on clean UI structure, reusable components, and modern frontend best practices.",
-        detaildescription: "This website was developed using Vue.js, adopting a clean and structured UI architecture and emphasizing component reusability. By leveraging this technology, we are able to build a modern, efficient, and easily maintainable web application.The creation of this website originated as a Final Project for a Bootcamp organized by NextSkill, titled “Learning Vue.js Fundamentals”. This project not only serves as a practical application of the skills acquired, but also marks the beginning of a professional portfolio in frontend development.",
-        mainImage: "SlicingVueJS.png",
-        images: [
-            ],
-        tags: ["Vue JS", "HTML", "Tailwind"],
-        github: "https://github.com/Aditya4348/SlicingWeb",
-        detail: [{category: "project", id: "3"}],
-    },
-    {
-        id: "4",
-        title: "Financial Web App",
-        description: "A financial management web application built with Laravel Livewire and Tailwind CSS, featuring real-time transaction tracking, income and expense reports, and role-based access control. Designed for simplicity, performance, and clarity in managing personal or team finances.",
-        detaildescription: "This website is the result of an independently established website development service, built using the PHP programming language and the Laravel framework. The development process was carried out professionally, with a focus on clean code structure, system efficiency, and ease of maintenance. The website uses Indonesian as the primary language, ensuring accessibility and clarity for its local target audience.",
-        
-        mainImage: "FinancialWeb.png",
-        images: [
-            ],
-        tags: ["Laravel", "LiveWire", "HTML", "CSS", "Tailwind"],
-        github: "https://github.com/Aditya4348/Financial_management_website",
-        detail: [{category: "project", id: "4"}],
-    },
-]
+  {
+    id: "1",
+    title: "Todolist Web",
+    description:
+      "A dynamic Todo List application built with Laravel Livewire and Tailwind CSS for real-time task management.",
+    detaildescription:
+      "This Todo List app was developed as a modern task management solution, focused on performance, usability, and responsive design. Built with Laravel Livewire, it delivers real-time interactivity without the need for additional JavaScript.All core features such as adding, editing, completing, and deleting tasks are handled without page reloads, ensuring a fast and seamless user experience — ideal for individuals or small teams.The UI is styled using Tailwind CSS, resulting in a clean, minimalist interface that looks great on all screen sizes. Responsiveness and speed were key priorities throughout the development.This project also serves as an exploration into building backend-driven SPA-like applications without relying on frontend frameworks like React or Vue. It showcases how Laravel can power interactive, modern web apps on its own.More than just a productivity tool, this app is a demonstration of my ability to build scalable, interactive web applications with clean architecture and modern tech stacks.",
+    mainImage: "TodolistWeb.png",
+    images: [],
+    tags: ["Laravel", "LiveWire", "HTML", "CSS", "Tailwind"],
+    github: "https://github.com/Aditya4348/Todolist",
+    detail: [{ category: "project", id: "1" }],
+  },
+  {
+    id: "2",
+    title: "Slicing Web ( React )",
+    description:
+      "A modern website layout sliced and built using React.js, component reusability, and clean UI architecture.",
+    detaildescription:
+      "A modern website built using React and TypeScript technologies, with a focus on component reusability and clean UI architecture.",
+    mainImage: "SlicingReact.png",
+    images: [],
+    tags: ["React", "Tailwind CSS", "HTML", "CSS", "Typescript", "Vite"],
+    github: "https://github.com/Aditya4348/SlicingWebReact",
+    detail: [{ category: "project", id: "2" }],
+  },
+  {
+    id: "3",
+    title: "Slicing Web ( Vue JS )",
+    description:
+      "A responsive website layout sliced and implemented using Vue.js and Tailwind CSS, focusing on clean UI structure, reusable components, and modern frontend best practices.",
+    detaildescription:
+      "This website was developed using Vue.js, adopting a clean and structured UI architecture and emphasizing component reusability. By leveraging this technology, we are able to build a modern, efficient, and easily maintainable web application.The creation of this website originated as a Final Project for a Bootcamp organized by NextSkill, titled “Learning Vue.js Fundamentals”. This project not only serves as a practical application of the skills acquired, but also marks the beginning of a professional portfolio in frontend development.",
+    mainImage: "SlicingVueJS.png",
+    images: [],
+    tags: ["Vue JS", "HTML", "Tailwind"],
+    github: "https://github.com/Aditya4348/SlicingWeb",
+    detail: [{ category: "project", id: "3" }],
+  },
+  {
+    id: "4",
+    title: "Financial Web App",
+    description:
+      "A financial management web application built with Laravel Livewire and Tailwind CSS, featuring real-time transaction tracking, income and expense reports, and role-based access control. Designed for simplicity, performance, and clarity in managing personal or team finances.",
+    detaildescription:
+      "This website is the result of an independently established website development service, built using the PHP programming language and the Laravel framework. The development process was carried out professionally, with a focus on clean code structure, system efficiency, and ease of maintenance. The website uses Indonesian as the primary language, ensuring accessibility and clarity for its local target audience.",
+
+    mainImage: "FinancialWeb.png",
+    images: [],
+    tags: ["Laravel", "LiveWire", "HTML", "CSS", "Tailwind"],
+    github: "https://github.com/Aditya4348/Financial_management_website",
+    detail: [{ category: "project", id: "4" }],
+  },
+  {
+    id: "5",
+    title: "Web App Generator Surat Organisasi",
+    description:
+      "A web-based application designed to quickly and easily generate official letters for Karang Taruna (youth organizations). This tool simplifies administrative tasks by allowing users to create, preview, and export professional letters in PDF format directly from their browser, with no server or login required.",
+    detaildescription:
+      "This project was developed as a modern frontend application using Vite, React, and TypeScript. It is designed to be a fully client-side tool, utilizing the browser's local storage for data persistence, which eliminates the need for a backend server or user authentication. The focus was on creating a fast, responsive, and user-friendly interface for efficient letter generation, complete with real-time previews. Additionally, the website was built and refined using Vibe Code, ensuring a smooth and streamlined development experience.",
+
+    mainImage: "SuratGenerator.png",
+    images: [],
+    tags: ["Vite", "React", "TypeScript", "Tailwind CSS", "HTML", "CSS", "Visual Studio Code"],
+    github: "https://torin-generator.vercel.app/",
+    detail: [{ category: "project", id: "5" }],
+  },
+];
